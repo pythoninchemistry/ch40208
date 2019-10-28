2a4edf52706ce6579afb92e2560be1758919ced7
Andrew McCluskey <andrew.mccluskey@diamond.ac.uk>
2019-10-28 10:33:34 +0000

    exercise for week5

---
+++ week_5/exercise/functions.py
@@ -0,0 +1,31 @@
+# Function for test 1
+from scipy.constants import c, h
+
+def energy(wavenumber):
+    return None
+
+
+# Function for test 2
+from scipy.constants import R
+
+def ideal_gas_law(number_of_moles, temperature, pressure):
+    return None
+
+
+# Function for test 3
+
+def allowed_angular_momentum_quantum_numbers(principal_quantum_number):
+    return None
+
+
+# Function for test 4
+
+def arrhenius_equation(pre_exponential_factor, activation_energy, 
+                       temperature):
+    return None
+
+
+# Function for test 5
+
+def morse(dissociation_energy, r, r_e):
+    return None
